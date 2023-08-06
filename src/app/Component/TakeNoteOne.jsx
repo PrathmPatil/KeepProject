0b97f53aad0e3316fb1088d0f29e431744b54252
prathm patil <prathmpatil2818@gmail.com>
2023-08-06 12:27:35 +0530

    TakeNoteOne is Done

---
+++ src/app/Component/TakeNoteOne.jsx
@@ -0,0 +1,36 @@
+"use client"
+import BrushIcon from '@mui/icons-material/BrushOutlined';
+import CheckBoxIcon from '@mui/icons-material/CheckBoxOutlined';
+import ImageIcon from '@mui/icons-material/ImageOutlined';
+
+
+
+function TakeNoteOne() {
+    
+    return (
+        <div className='p-2 mb-8 mb-8 h-[7vh]
+        xs:ml-[0vw] xs:w-[100%] max:sm:ml-[0] max:sm:w-[100%] sm:ml-[0vw] sm:w-[90%] md:ml-[2vw] md:w-[88%] lg:ml-[7vw] lg:w-[100%]  xl:w-[54%] xl:ml-[13vw] xl:w-[30vw] 2xl:w-[20vw]  
+         rounded-lg border-1 border-slate-400 shadow-md'>
+            <div className=' h-full w-full   flex justify-between items-center px-2' >
+                <input
+                    className='h-full w-[72%] text-slate-700 focus:outline-none text-md' placeholder='Take a Note....'
+                    contenteditable="true"
+                />
+
+                <div className=' h-[7vh] w-[28%] flex justify-between items-center'>
+                    <div className='h-full w-[25%]  flex  items-center  justify-center  hover:bg-slate-300 rounded-[50%]  text-slate-500 hover:text-slate-800'>
+                        <CheckBoxIcon />
+                    </div>
+                    <div className='h-full  w-[25%]  flex  items-center  justify-center  hover:bg-slate-300 rounded-[50%]  text-slate-500 hover:text-slate-800'>
+                        <BrushIcon />
+                    </div>
+                    <div className='h-full  w-[25%] flex  items-center  justify-center  hover:bg-slate-300 rounded-[50%]  text-slate-500 hover:text-slate-800'>
+                        <ImageIcon />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default TakeNoteOne
--- src/app/Dashboard/page.jsx
+++ src/app/Dashboard/page.jsx
@@ -1,7 +1,9 @@
 "use client"
 import React from 'react'
+import { Box } from '@mui/material';
 import Header1 from '../Component/Header';
 import MenuBar from '../Component/MenuBar';
+import TakeNoteOne from '../Component/TakeNoteOne';
 
 function Dashboard() {
     // open side bar
@@ -20,6 +22,11 @@ function Dashboard() {
     <div className='relative'>
       <Header1 handleDrawer={handleDrawer} ChangeFlex={ChangeFlex} />
       <MenuBar handleDrawer={handleDrawer} open={open} />
+      <Box className='absolute top-20 right-0 flex flex-col'
+        sx={{ width: open ? ['76vw'] : ['83vw'] }}
+      >
+        <TakeNoteOne/>
+      </Box>
     </div>
   )
 }
